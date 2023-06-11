1cdc6451c666f9a9cf49d73fabdf08b6b02c6a2c
Soham Rajput <sohamrajput7@gmail.com>
2023-06-10 20:48:01 -0400

    updated 04-tips-and-tricks

--- 04-tips-and-tricks/live/global/iam-for-each/outputs.tf
+++ 04-tips-and-tricks/live/global/iam-for-each/outputs.tf
@@ -1,3 +1,9 @@
+# values are all the outputs for that resource.
 output "all_users" {
   value = aws_iam_user.example
+}
+
+# values are all the arns for that resource.
+output "all_arns" {
+  value = values(aws_iam_user.example)[*].arn
 }
